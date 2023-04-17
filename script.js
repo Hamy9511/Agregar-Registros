15439608ad57f58d745395a21e5ff3d43faecb76
Hamilton Durán <hamy.duran@gmail.com>
2023-04-16 18:20:33 -0600

    Clase 03 completada - se agregaron funciones de agregar una lista al HTML por medio de JS

--- script.js
+++ script.js
@@ -4,7 +4,22 @@ const  createTask = (evento) =>{ //Arrow function o Funciones anonimas  //Liaten
         //listener
         evento.preventDefault(); //Para no refrescar la pantalla
         const input = document.querySelector("[data-form-input]");
-        console.log(input.value);
+        const value = input.value; 
+        const task = document.createElement("li");
+        const list = document.querySelector("[data-list]");
+        task.classList.add("card"); //agregammos una nueva clase
+        input.value = " "; //Al agregar una tarea se borre el texto del input
+        //HACER QUE JS AGREGUE CONTENIDO AL DOM
+        const content = `  
+        <div>
+        <i class="far fa-check-square icon"></i>
+        <span class="task">${value /*este comando indica que es un commando JS en HTML*/}</span> 
+        </div>
+        <i class="fas fa-trash-alt trashIcon icon"></i>`
+        task.innerHTML = content; //Aqui le digo que quiero agregar al HTML la variable content
+
+        list.appendChild(task);
+        console.log(content);
     }
 
 btn.addEventListener("click", createTask); //Lllamando a la funcion para hacer el listener
