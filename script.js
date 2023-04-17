30c8c8d23388f0288efc40ba5dcf7dbc1775e2cd
Hamilton Durán <hamy.duran@gmail.com>
2023-04-16 20:45:31 -0600

    clase03 terminada - configuramos la insercion de la tarea en la lista

--- script.js
+++ script.js
@@ -10,16 +10,24 @@ const  createTask = (evento) =>{ //Arrow function o Funciones anonimas  //Liaten
         task.classList.add("card"); //agregammos una nueva clase
         input.value = " "; //Al agregar una tarea se borre el texto del input
         //HACER QUE JS AGREGUE CONTENIDO AL DOM
-        const content = `  
-        <div>
-        <i class="far fa-check-square icon"></i>
-        <span class="task">${value /*este comando indica que es un commando JS en HTML*/}</span> 
-        </div>
-        <i class="fas fa-trash-alt trashIcon icon"></i>`
-        task.innerHTML = content; //Aqui le digo que quiero agregar al HTML la variable content
-
+        const taskContent = document.createElement("div"); //creo el div
+        taskContent.appendChild(checkComplete()); //inserto dentro del div la fucnion
+        const titleTask = document.createElement("span"); //creo la etiqueta span
+        titleTask.classList.add("task"); //Lo llamo con el id task
+        titleTask.innerText = value; //inserto en el span el valor de value
+        taskContent.appendChild(titleTask);
+        const content = `<i class="fas fa-trash-alt trashIcon icon"></i>`
+        task.appendChild(taskContent);
         list.appendChild(task);
         console.log(content);
     }
 
 btn.addEventListener("click", createTask); //Lllamando a la funcion para hacer el listener
+
+const checkComplete = () => {
+    const i = document.createElement("i");
+    i.classList.add("far");
+    i.classList.add("fa-check-square");
+    i.classList.add("icon");
+    return i;
+}
